cd2db2551c978ef20a1e8445dc67f847c3125129
Madhu <madhuvanmahesh@gmail.com>
2016-02-02 19:40:45 -0800

    Write tests for suite 'RSS Feeds'
    Write 3 specs for the suite 'RSS Feeds'. Specs are 'are defined', 'URLs are defined' and 'names are defined'.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,29 +31,45 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('URLs are defined', function(){
+            for(var i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i]).toBeDefined();
+                expect(allFeeds[i].url).not.toBe(null);
+            }
+        });
+
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('names are defined', function(){
+            for(var i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name).not.toBe(null);
+            }
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is hidden by default', function(){
+            expect(allFeeds[i].name).toBeDefined();
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
